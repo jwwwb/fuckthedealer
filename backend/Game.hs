7a1fec8fee5720f2ee279bc40d8fa7e2f20490e5
jwwwb <james@jwwwb.com>
2020-05-03 20:08:34 +0200

    Adds simple server with root and guess endpoint

--- backend/Game.hs
+++ backend/Game.hs
@@ -1,5 +1,10 @@
 module Game( newGame
+           , GameState
+           , readValue
            , numCards
+           , jsonPlayer
+           , jsonDealer
+           , guess
            ) where
 import Data.List
 
@@ -16,6 +21,12 @@ numericSuit Spades = 0
 numericSuit Clubs = 1
 numericSuit Diamonds = 2
 numericSuit Hearts = 3
+
+showSuit :: Suit -> String
+showSuit Spades = "s"
+showSuit Hearts = "h"
+showSuit Clubs = "c"
+showSuit Diamonds = "d"
 
 data Value = Two | Three | Four | Five |
              Six | Seven | Eight | Nine |
@@ -39,6 +50,37 @@ numericValue Jack = 11
 numericValue Queen = 12
 numericValue King = 13
 numericValue Ace = 14
+
+showValue :: Value -> String
+showValue Two = "2"
+showValue Three = "3"
+showValue Four = "4"
+showValue Five = "5"
+showValue Six = "6"
+showValue Seven = "7"
+showValue Eight = "8"
+showValue Nine = "9"
+showValue Ten = "t"
+showValue Jack = "j"
+showValue Queen = "q"
+showValue King = "k"
+showValue Ace = "a"
+
+readValue :: String -> Maybe Value
+readValue "2" = Just Two
+readValue "3" = Just Three
+readValue "4" = Just Four
+readValue "5" = Just Five
+readValue "6" = Just Six
+readValue "7" = Just Seven
+readValue "8" = Just Eight
+readValue "9" = Just Nine
+readValue "t" = Just Ten
+readValue "j" = Just Jack
+readValue "q" = Just Queen
+readValue "k" = Just King
+readValue "a" = Just Ace
+readValue other = Nothing
 
 numValues :: Int
 numValues = length values
@@ -100,13 +142,42 @@ dealerPunishment SecondGuess = 2
 
 data Punishment = Punishment Int Int
 
+currentCard :: GameState -> Card
+currentCard game = (deck game !! uncovered game)
+
 currentValue :: GameState -> Value
-currentValue game = getValue ((deck game) !! (uncovered game))
+currentValue game = getValue (currentCard game)
+
+showCard :: Card -> String
+showCard (Card value suit) = showValue value ++ showSuit suit
+
+join :: String -> String -> String
+join a b = a ++ ", " ++ b
+
+showCards :: Deck -> String
+showCards [] = ""
+showCards [single] = "\"" ++ (showCard single) ++ "\""
+showCards deck = foldl1 join (map (\c -> "\"" ++ showCard c ++ "\"") deck)
+
+jsonPlayer :: GameState -> String
+jsonPlayer game = "{\"revealed\": " ++ revealed ++ "}"
+  where revealed = "[" ++ individuals ++ "]"
+        individuals = showCards (take (uncovered game) (deck game))
+
+jsonDealer :: GameState -> String
+jsonDealer game = "{\"revealed\": " ++ revealed ++
+                  ", \"current\": \"" ++ current ++ "\"}"
+  where revealed = "[" ++ individuals ++ "]"
+        individuals = showCards (take (uncovered game) (deck game))
+        current = showCard (currentCard game)
 
 inc :: Wrongs -> Wrongs
 inc ZeroWrongs = OneWrong
 inc OneWrong = TwoWrongs
 inc TwoWrongs = ZeroWrongs
+
+pass :: [String] -> Int -> Int
+pass players number = (number + 1) `mod` (length players)
 
 updateGuesses :: Guesses -> Bool -> Guesses
 updateGuesses guesses correct = case (correct, (guessNumber guesses)) of
@@ -125,8 +196,28 @@ shouldUpdateDealer guesses correct =
 
 guess :: GameState -> Value -> (GameState, Punishment)
 guess game card = (nextGame, punishment)
-  where nextGame = game
-        punishment = case missesBy card (currentValue game) of
+  where nextGame = GameState { deck = (deck game)
+                             , uncovered = nextUncovered
+                             , players = (players game)
+                             , dealer = nextDealer
+                             , guesses = nextGuesses
+                             , guesser = nextGuesser }
+        punishment = case miss of
                        0 -> Punishment (dealer game) dealerPun
                        val -> Punishment (guesser game) val
+        nextGuesses = updateGuesses (guesses game) correct
+        nextUncovered = case (correct, (guessNumber (guesses game))) of
+                          (False, FirstGuess) -> uncovered game
+                          _ -> (uncovered game) + 1
+        nextGuesser = case (correct, (guessNumber (guesses game))) of
+                        (False, FirstGuess) -> guesser game
+                        _ -> case (pass' (guesser game)) == nextDealer of
+                               True -> pass' (pass' (guesser game))
+                               False -> pass' (guesser game)
+        nextDealer = case shouldUpdateDealer (guesses game) correct of
+                       True -> pass' (dealer game)
+                       False -> dealer game
+        pass' = pass (players game)
+        correct = miss == 0
+        miss = missesBy card (currentValue game)
         dealerPun = dealerPunishment (guessNumber (guesses game))
--- backend/Main.hs
+++ backend/Main.hs
@@ -1,7 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
-import qualified Game
+
 import Control.Monad
+import Control.Monad.IO.Class (liftIO)
+
+import Data.Text
+import Data.IORef
+
+import qualified Game
+
+import Network.HTTP.Types
+
 import System.Random
+
+import Web.Spock
+import Web.Spock.Config
+
 
 minDie :: Int
 minDie = 0
@@ -9,9 +23,36 @@ minDie = 0
 maxDie :: Int
 maxDie = 999
 
+newtype ServerState = ServerState { game :: IORef Game.GameState }
+
+-- SpockM takes connection, session and state type params:
+type Server a = SpockM () () ServerState a
+
+updateGame :: Game.GameState -> String -> Game.GameState
+updateGame game guess = case (Game.readValue guess) of
+  -- just ignore the punishment for now
+  Just card -> fst (Game.guess game card)
+  Nothing -> game
+
+app :: Server ()
+app = do
+  get root $ do
+    game' <- getState >>= (liftIO . readIORef . game)
+    text (pack (Game.jsonPlayer game'))
+  get ("guess" <//> var) $ \guess -> do
+    gameRef <- game <$> getState
+    liftIO $ atomicModifyIORef' gameRef $ \game ->
+      (updateGame game guess, ())
+    redirect "/"
+
+
 main :: IO ()
 main = do
   seed <- forM [1..Game.numCards] $ \i -> do
     randomRIO (minDie, maxDie)
   let game = Game.newGame seed ["Alice", "Bob", "Charlie"]
-  putStrLn (show game)
+
+  st <- ServerState <$> newIORef game
+  cfg <- defaultSpockCfg () PCNoDatabase st
+
+  runSpock 8080 (spock cfg app)
